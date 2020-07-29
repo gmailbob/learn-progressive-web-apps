171ae5a0403feeb9ede4d558272994f9dc3acee1
Zhiyu Zhou <Zhiyu.Zhou@adp.com>
2020-07-29 09:05:40 -0400

    defer install to home screen banner

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,15 @@
+var deferredPrompt;
+
 if('serviceWorker' in navigator){
   navigator.serviceWorker.register('/sw.js')
     .then(()=>{
       console.log('service worker registered.')
     });
 }
+
+addEventListener('beforeinstallprompt', event=>{
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+})
