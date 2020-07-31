cfeb2a7604311b1288335b559ec34cd6669a0462
Zhiyu Zhou <Zhiyu.Zhou@adp.com>
2020-07-30 23:10:48 -0400

    cache static file content

--- sw.js
+++ sw.js
@@ -1,5 +1,23 @@
 self.addEventListener("install", function(event) {
   console.log("[Service Worker] Installing Service Worker ...", event);
+  /**
+   * The install events in service workers use waitUntil() to hold the service
+   * worker in the installing phase until tasks complete. If the promise passed
+   * to waitUntil() rejects, the install is considered a failure, and the
+   * installing service worker is discarded. This is primarily used to ensure
+   * that a service worker is not considered installed until all of the core
+   * caches it depends on are successfully populated.
+   */
+  event.waitUntil(
+    caches.open("static").then(cache => {
+      console.log("service worker precaching App Shell");
+      // although the file path is used, the 'key' will be the request and
+      // 'value' will be the file content; see how we use .match() below
+      cache.add("/"); // cache root request
+      cache.add("/index.html");
+      cache.add("/src/js/app.js");
+    })
+  );
 });
 
 self.addEventListener("activate", function(event) {
@@ -8,5 +26,10 @@ self.addEventListener("activate", function(event) {
 });
 
 self.addEventListener("fetch", function(event) {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) return response; // cache hit, otherwise it's null
+      return fetch(event.request);
+    })
+  );
 });
