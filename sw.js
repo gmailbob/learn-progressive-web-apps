03b13df80fb8571cb6cc660893308cfad78cbf59
Zhiyu Zhou <Zhiyu.Zhou@adp.com>
2020-07-30 23:26:45 -0400

    use cache.addAll()

--- sw.js
+++ sw.js
@@ -13,9 +13,21 @@ self.addEventListener("install", function(event) {
       console.log("service worker precaching App Shell");
       // although the file path is used, the 'key' will be the request and
       // 'value' will be the file content; see how we use .match() below
-      cache.add("/"); // cache root request
-      cache.add("/index.html");
-      cache.add("/src/js/app.js");
+      cache.addAll([
+        "/", // cache root request
+        "/index.html",
+        "/src/js/app.js",
+        "/src/js/feed.js",
+        "/src/js/promise.js", // a better way is to conditionally 'include' polyfill files, since modern browsers who support service worker should also support promise/fetch and do not need these files
+        "/src/js/fetch.js",
+        "/src/js/material.min.js",
+        "/src/css/app.css",
+        "/src/css/feed.css",
+        "/src/images/main-image.jpg",
+        "https://fonts.googleapis.com/css?family=Roboto:400,700",
+        "https://fonts.googleapis.com/icon?family=Material+Icons",
+        "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css"
+      ]);
     })
   );
 });
